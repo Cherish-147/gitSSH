1c2c9c99453a6b7e2346861c035ecfd573096451
hwman74 <hwman@qq.com>
2023-03-22 19:59:26 +0800

    使用会话属性实现会话

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,8 +3,11 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes({"username"})
 public class HomeController {
     @RequestMapping("/hello")
     String hello(String username, Model model)
