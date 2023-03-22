c81a9ca4039b7813be961ce950a6e684004ed7f1
hwman74 <hwman@qq.com>
2023-03-22 20:04:35 +0800

    使用 HttpSession参数实现会话

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,19 +6,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
-@SessionAttributes({"username"})
 public class HomeController {
     @RequestMapping("/hello")
-    String hello(String username, Model model)
+    String hello(String username, Model model, HttpSession session)
     {
         model.addAttribute("username", username);
+        session.setAttribute("username", username);
         return "hello";
     }
 
     @RequestMapping("/getuser")
-    String GetUser(Model model)
+    String GetUser(Model model, HttpSession session)
     {
+        String username = (String)session.getAttribute("username");
+        model.addAttribute("username", username);
+        return "hello";
+    }
+
+    @RequestMapping("/logout")
+    String Logout(HttpSession session)
+    {
+        session.invalidate(); //销毁会话
         return "hello";
     }
 
